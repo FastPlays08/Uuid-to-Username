d2302a3ad4b4452b6c8002436cf1b0ee9a3327f0
FastPlays08 <fastplays08@outlook.com>
2025-01-10 15:43:19 +1100

    Update

--- Converter.py
+++ Converter.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python3
-
 from UuidToName import *
 
-with open('uuids.txt') as fp:
-    for line in fp:
-        uuid = line[:-5]
-        print(uuid)
-        print(usernameFromUuid(uuid))
+mode = input("Mode?\n1. uuid:username\n2. username\nDefault=1\n")
+
+try:
+    mode = int(mode)
+except ValueError:
+    pass
+
+with open('uuids.txt') as uuids, open('output.txt','a') as out_file:
+    if mode == 2:
+        for line in uuids:
+            if line.endswith(".dat\n"):
+                uuid = line[:-5]
+            else:
+                print("Line is not a .dat file, passing")
+                continue
+            out_file.write(usernameFromUuid(uuid)+"\n")
+        print("Done!")
+    else:
+        for line in uuids:
+            if line.endswith(".dat\n"):
+                uuid = line[:-5]
+            else:
+                print("Line is not a .dat file, passing")
+                continue
+            username = usernameFromUuid(uuid)
+            out_file.write(f'{uuid}:{username}\n')
+        print("Done!")
