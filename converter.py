2cf261ab5d4ea4b328654e170b30bca60ff1fe89
FastPlays08 <fastplays08@outlook.com>
2025-04-03 00:00:09 +1100

    Add function to retrieve UUID from username and update main execution flow in converter.py

--- converter.py
+++ converter.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
-import re
 import requests
-import json  # Import the json module
+import json
 
-def usernameFromUuid(uuid):
+def usernameFromUuid(uuid: str):
     response_text = requests.get(f'https://api.minecraftservices.com/minecraft/profile/lookup/{uuid}').text
     response_json = json.loads(response_text)
     return response_json.get("name")
 
+def uuidFromUsername(username: str):
+    response_text = requests.get(f"https://api.minecraftservices.com/minecraft/profile/lookup/name/{username}").text
+    response_json = json.loads(response_text)
+    return response_json.get("id")
+
 if __name__ == "__main__":
-    uuid = "404912f3-fa81-41e4-a135-08f80e3df0a5"
-    player_uuids = ["404912f3-fa81-41e4-a135-08f80e3df0a5", "ffc9bf4f-e16e-43fb-848b-1450a2bd0390"]
+    name = "fastplays08"
+    print(uuidFromUsername(name))
+    player_uuids = ["404912f3-fa81-41e4-a135-08f80e3df0a5", "ffc9bf4fe16e43fb848b1450a2bd0390"]
     for i in player_uuids:
         print(usernameFromUuid(i))
+
+    
